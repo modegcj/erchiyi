2fef508a167cfaa3c666e0182fb07627c12e2274
gaochunjin <15106846927@163.com>
2022-05-23 11:07:58 +0800

    第一次

---
+++ ts-study/src/views/About.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="about">
+    <h1>{{msg}}</h1>
+    <div class="box">
+      这是一个块元素
+      <span class="span1">这是一个行内元素1</span>
+      <span class="span2">这是一个行内元素1</span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { Ref } from "@vue/reactivity";
+import { onMounted, ref } from 'vue';
+
+let msg:Ref = ref<string>('This is an about page');
+class Animal{
+  name:string;
+  constructor(name:string){
+    this.name = name;
+  }
+  do(){
+    return this.name
+  }
+}
+class Dog extends Animal{
+  color:string;
+  size:string;
+  constructor(name:string,color:string,size:string){
+    super(name);
+    this.color = color;
+    this.size = size;
+  }
+  greet(){
+    return this.color+';'+this.size;
+  }
+}
+let xiaohei = new Dog('小黑','red','big');
+// console.log(xiaohei.do(),1);
+// console.log(xiaohei.greet(),2);
+onMounted(()=>{
+  // console.log(xiaohei);
+  let myAdd:(baseValue:number,increment:number) => number = function(x,y){return x+y};
+  // console.log(myAdd(3,7))
+
+  let myReduce = (baseValue:number,increment:number):number=>{
+    return baseValue-increment
+  }
+  
+  // console.log(myReduce(7,3));
+  let a:number = 12;
+  let b:string = a.toString();
+  console.log(b)
+})
+</script>
+<style lang="scss" scoped>
+.box{
+  .span1{
+    color: red;
+  }
+  &:hover{
+    color: blue;
+  }
+}
+</style>
---
+++ ts-study/src/views/study/BoxFore.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="fore">
+        <h1>第四页</h1>
+    </div>
+</template>
+<script lang="ts" setup>
+</script>
+<style>
+</style>
