24d24187bc23106fa7bba9738c0795702f8298c0
gaochunjin <15106846927@163.com>
2022-06-06 09:16:34 +0800

    更改

--- src/views/study/BoxTwo.vue
+++ src/views/study/BoxTwo.vue
@@ -4,6 +4,35 @@
     </div>
 </template>
 <script lang="ts" setup>
+import { onMounted } from "vue";
+
+// let a = [1,'1'];
+// a = [];
+// console.log(a);
+
+let extend = ():number=>{
+    return 123;
+}
+
+onMounted(()=>{
+    class twoObj{
+        name?:string;
+        do(a:string){
+            this.name = a;
+        }
+    }
+    let two = new twoObj();
+    let t = new twoObj();
+    // console.log(two.name,t.name);
+    two.do('twoooooo');
+    t.do('toooo');
+    // console.log(two.name,t.name);
+    let identity = <T>(arg:T):T =>{
+        return arg;
+    }
+    let output = identity('string');
+    // console.log(output);
+})
 </script>
 <style>
 </style>
