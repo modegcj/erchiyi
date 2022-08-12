2481edab9a7bd59abb23fd3f2d84ac9cb43966fd
gaochunjin <15106846927@163.com>
2022-08-12 16:38:07 +0800

    定义类型

--- src/views/study/BoxFour.vue
+++ src/views/study/BoxFour.vue
@@ -4,6 +4,14 @@
     </div>
 </template>
 <script lang="ts" setup>
+// 定义类型
+type TNull<T> = T|null;
+let a:TNull<number> = 123;
+let b:TNull<number> = null
+console.log(a,b);
+type UNull<U> = (arg:U) => U|null;
+let c:UNull<number> = (n)=>null;
+console.log(c(123))
 </script>
 <style>
 </style>
